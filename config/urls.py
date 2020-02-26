1aa53c649be7d691b5d3d250860369a511a0586a
sami <ydjm1027@naver.com>
2020-02-26 23:46:05 +0900

    review

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
-    path("", include("core.urls", namespace = "core")),
+    path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
     path("users/", include("users.urls", namespace="users")),
+    path("reservations/", include("reservations.urls", namespace="reservations")),
+    path("reviews/", include("reviews.urls", namespace="reviews")),
     path("admin/", admin.site.urls),
 ]
 
